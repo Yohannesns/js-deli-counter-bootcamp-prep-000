067e39185169d2a95d575e7fcb3a0e74c4152a7c
Yohannes Abera <yohannesns@gmail.com>
2018-06-02 23:06:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+var katzDeliLine = [];
+
+
+function takeANumber(katzDeliLine,name){
+katzDeliLine.push(name)
+return("Welcome, " +  name + "." + " You are number" + " " + (parseInt(katzDeliLine.indexOf(name))+ 1) + " in line.")
+
+
+}
+function nowServing (katzDeliLine){
+  if(katzDeliLine.length > 0 ){
+    return  "Currently serving " +  katzDeliLine.shift() + "."
+  }
+   return "There is nobody waiting to be served!"
+  }
+
+  function currentLine(katzDeliLine){
+  	  if(katzDeliLine.length < 1 || katzDeliLine === undefined){
+  	  return "The line is currently empty."
+  	  }
+  	 for(var i=0; i < katzDeliLine.length; i++){
+       
+
+       var str =  katzDeliLine.toString()
+      
+
+
+      }
+      {
+  	  return  "The line is currently: " + str
+      	  }
+  	  }
